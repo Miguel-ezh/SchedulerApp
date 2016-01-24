24d8c37b66e06a28e6df93ce82fa6758a224f380
Miguel Zaldivar <miguel.ezh@gmail.com>
2016-01-24 01:12:09 -0500

    jspm first commit

--- app.js
+++ app.js
@@ -5,14 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'app'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -21,9 +19,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/app', express.static(path.join(__dirname, 'app')));
+app.use('/jspm_packages', express.static(path.join(__dirname, 'jspm_packages')));
+app.use('/public/config.js', express.static(path.join(__dirname, 'config.js')));
+
+var routes = require('./routes/index');
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ app/boot.js
@@ -0,0 +1,52 @@
+import 'angular-material.css!'
+
+import angular from 'angular'
+import material from 'angular-material'
+import 'angular-ui-router'
+
+// Load loggers for injection and pre-angular debugging
+
+import { LogDecorator, ExternalLogger } from 'app/utils/LogDecorator';
+
+/**
+ * Manually bootstrap the application when AngularJS and
+ * the application classes have been loaded.
+ */
+angular
+  .element( document )
+  .ready( function() {
+
+    let appName = 'schedulerApp';
+    let $log = new ExternalLogger();
+
+    $log = $log.getInstance( "BOOTSTRAP" );
+    $log.debug( "Initializing '{0}'", [ appName ] );
+    
+    let body = document.getElementsByTagName("body")[0];
+    let app  = angular
+          .module( appName, [ 
+            'ui.router',
+            material
+            ])
+          .run(
+          [
+              '$rootScope', '$state', '$stateParams',
+            function ($rootScope, $state, $stateParams) {
+                // It's very handy to add references to $state and $stateParams to the $rootScope
+                // so that you can access them from any scope within your applications.For example,
+                // <li ng-class="{ active: $state.includes('contacts.list') }"> will set the <li>
+                // to active whenever 'contacts.list' or one of its decendents is active.
+                $rootScope.$state = $state;
+                $rootScope.$stateParams = $stateParams;
+                $rootScope.$on("$stateChangeError", console.log.bind(console));
+            }
+          ])
+          .config( ['$provide', '$stateProvider', '$urlRouterProvider', '$mdThemingProvider', 
+                    function($provide, $stateProvider, $urlRouterProvider, $mdThemingProvider){
+                $urlRouterProvider.otherwise('/app');
+                LogDecorator($provide);
+          }]);
+          
+    angular.bootstrap( body, [ app.name ], { strictDi: false })
+  });
+
