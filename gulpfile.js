fe8bb79091d8eda9e6c5225bc2edcd3dbeb72353
Miguel Zaldivar <miguel.ezh@gmail.com>
2016-01-31 15:04:02 -0500

    sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var server = require('gulp-express');
+var sass = require('gulp-sass');
 
 gulp.task('run-server', function () {
     // Start the server at the beginning of the task 
@@ -12,10 +13,21 @@ gulp.task('run-server', function () {
        ], [server.run]);
 });
 
-gulp.task('watch-app', function(){
+gulp.task('sass', function () {
+  gulp.src('./sass/base.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./public/stylesheets'));
+});
+
+gulp.task('app:watch', function(){
   gulp.watch(['app/index.html'], server.notify);
   gulp.watch(['app/**/*.html'], server.notify);
   gulp.watch(['app/**/*.js'], server.notify);
+  gulp.watch(['app/**/**/*.js'], server.notify);
 });
 
-gulp.task('serve', ['run-server','watch-app']);
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+gulp.task('serve', ['run-server','app:watch', 'sass:watch']);
